227e560636da830258fd6dd239fb439770d7a5eb
Markku Vimpari <markku.vimpari@wimapri.fi>
2021-05-20 12:56:37 +0300

    uMs and uMc specific commands added to sample.c

--- src/sample/sample.c
+++ src/sample/sample.c
@@ -34,8 +34,10 @@
 
 typedef struct params_s
 {
-    float x, y, z, d, X, Y, Z, D;
-    int verbose, update, loop, dev, speed, timeout;
+    float x, y, z, d, X, Y, Z, D, pressure_kpa;
+    int verbose, update, loop, dev, speed, timeout, value;
+    int calibrate_pressure, pressure_channel, valve_channel, reset_fluid_detector, pressure_sensor;
+    bool lens_position, read_fluid_detectors;
     char *address;
 } params_struct;
 
@@ -44,7 +46,7 @@ void usage(char **argv)
     fprintf(stderr,"usage: %s [opts]\n", argv[0]);
     fprintf(stderr,"Generic options\n");
     fprintf(stderr,"-d\tdev (def: %d)\n", DEV);
-    fprintf(stderr,"-v\tverbose\n");
+    fprintf(stderr,"-e\tverbose\n");
     fprintf(stderr,"-a\taddress (def: %s)\n", LIBUM_DEF_BCAST_ADDRESS);
     fprintf(stderr,"-u\tposition and status update period (def: %d ms)\n", UPDATE);
     fprintf(stderr,"-t\tcommand timeout (def: %d ms)\n", LIBUM_DEF_TIMEOUT);
@@ -59,6 +61,20 @@ void usage(char **argv)
     fprintf(stderr,"-W\tabs target for the 4th axis\n");
     fprintf(stderr,"-D\tabs target for the 4th axis, alias for above\n");
     fprintf(stderr,"-n\tcount\tloop between current and target positions or take multiple relative steps into same direction\n");
+
+    fprintf(stderr,"Get or set lens changer position\n");
+    fprintf(stderr,"-L\t\tto get or set lens position, set if -v defined\n");
+    fprintf(stderr,"-v\tvalue 0 for center, 1-X for positions\n");
+
+    fprintf(stderr,"Get or set uMc controls (give -v value to set)\n");
+    fprintf(stderr,"-C\tchn\tpressure channel 1-8 (0 to disable)\n");
+    fprintf(stderr,"-v\tvalue\tkPa, range -70.0 - +70.0\n");
+    fprintf(stderr,"-V\tchn\tvalve channel 1-8 (0 to disable)\n");
+    fprintf(stderr,"-v\tvalue\t0 or 1\n");
+    fprintf(stderr,"-R\tchn\tread pressure sensor, 1-8 (0 to disable)\n");
+    fprintf(stderr,"-r\tchn\treset/calibrate fluid detectors 1-8 (0 to disable)\n");
+    fprintf(stderr,"-U\t\tread fluid detectors\n");
+    fprintf(stderr,"-B\tchn\tpressure channel to be calibrated\n");
     exit(1);
 }
 
@@ -72,7 +88,9 @@ void parse_args(int argc, char *argv[], params_struct *params)
     params->Y = LIBUM_ARG_UNDEF;
     params->Z = LIBUM_ARG_UNDEF;
     params->D = LIBUM_ARG_UNDEF;
+    params->pressure_kpa = LIBUM_ARG_UNDEF;
     params->dev = DEV;
+    params->value = -1;
     params->update = UPDATE;
     params->address = LIBUM_DEF_BCAST_ADDRESS;
     params->timeout = LIBUM_DEF_TIMEOUT;
@@ -85,11 +103,14 @@ void parse_args(int argc, char *argv[], params_struct *params)
             case 'h':
                 usage(argv);
                 break;
-            case 'v':
+            case 'e':
                 params->verbose++;
                 break;
             case '1':
                 params->verbose = 0;
+                break;
+            case 'L':
+                params->lens_position = true;
                 break;
             case 'n':
                 if(i < argc-1 && sscanf(argv[++i],"%d",&v) == 1 && v > 0)
@@ -176,6 +197,51 @@ void parse_args(int argc, char *argv[], params_struct *params)
                 else
                     usage(argv);
                 break;
+
+            case 'v':
+                if(i < argc-1 && (sscanf(argv[++i],"0x%x",&v) == 1 ||
+                                  sscanf(argv[i],"%d",&v) == 1))
+                    params->value = v;
+                else
+                    usage(argv);
+                if(params->pressure_channel && sscanf(argv[i], "%f", &f) == 1)
+                    params->pressure_kpa = f;
+                break;
+
+            // uMc additions
+            case 'C':
+                if(i < argc-1 && sscanf(argv[++i],"%d",&v) == 1 && v >= 1 && v <= 8)
+                    params->pressure_channel = v;
+                else
+                    usage(argv);
+                break;
+            case 'U':
+                params->read_fluid_detectors = true;
+                break;
+            case 'V':
+                if(i < argc-1 && sscanf(argv[++i],"%d",&v) == 1 && v >= 1 && v <= 8)
+                    params->valve_channel = v;
+                else
+                    usage(argv);
+                break;
+            case 'R':
+                if(i < argc-1 && sscanf(argv[++i],"%d",&v) == 1 && v >= 1 && v <= 8)
+                    params->pressure_sensor = v;
+                else
+                    usage(argv);
+                break;
+            case 'B':
+                if(i < argc-1 && sscanf(argv[++i],"%d",&v) == 1 && v >= 1 && v <= 8)
+                    params->calibrate_pressure = v;
+                else
+                    usage(argv);
+                break;
+            case 'r':
+                if(sscanf(argv[++i],"%d",&v) == 1 && v >= 0 && v <= 8)
+                    params->reset_fluid_detector = v;
+                else
+                    usage(argv);
+                break;
             default:
                 usage(argv);
                 break;
@@ -197,7 +263,7 @@ int main(int argc, char *argv[])
 {
     um_state *handle = NULL;
     int ret, status, loop = 0;
-    float home_x = 0.0, home_y = 0.0, home_z = 0.0, home_d = 0.0;
+    float pressure_kpa, home_x = 0.0, home_y = 0.0, home_z = 0.0, home_d = 0.0;
     params_struct params;
 
     parse_args(argc, argv, &params);
@@ -210,6 +276,163 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    // uMs features, get or set lens/objective position
+    if(params.lens_position)
+    {
+        if((ret = ums_get_lens_position(handle, params.dev)) < 0)
+            fprintf(stderr, "Get lens position failed - %s\n", um_last_errorstr(handle));
+        else
+        {
+            if(params.value >= 0)
+            {
+                printf("Lens position was %d\n", ret);
+                // 5mm move away, no dip
+                if((ret = ums_set_lens_position(handle, params.dev,  params.value, 5000, 0)) < 0)
+                    fprintf(stderr, "Set lens position failed - %s\n", um_last_errorstr(handle));
+                else
+                    printf("Lens position now %d\n", params.value);
+            }
+            else
+                printf("Lens position %d\n", ret);
+        }
+        um_close(handle);
+        exit(ret>=0?0:ret);
+    }
+
+    //uMc features
+    if(params.pressure_channel)
+    {
+        do
+        {
+            if((ret = umc_get_pressure_setting(handle, params.dev, params.pressure_channel, &pressure_kpa)) < 0)
+                fprintf(stderr, "Get pressure setting failed - %s\n", um_last_errorstr(handle));
+            else
+                printf("Channel %d pressure setting: %1.3f kPa\n", params.pressure_channel, pressure_kpa);
+            if(!params.loop)
+                break;
+            if(loop < params.loop-1)
+                um_receive(handle, params.update);
+        }
+        while(++loop < params.loop);
+
+        if(ret >= 0 && !isnan(params.pressure_kpa))
+        {
+            if((ret = umc_set_pressure_setting(handle, params.dev, params.pressure_channel, params.pressure_kpa)) < 0)
+                fprintf(stderr, "Set pressure failed - %s\n", um_last_errorstr(handle));
+            else
+                printf("Channel %d pressure set: %1.3f kPa\n", params.pressure_channel, params.pressure_kpa);
+        }
+        um_close(handle);
+        exit(ret>=0?0:ret);
+    }
+
+    if(params.valve_channel)
+    {
+        do
+        {
+            if((ret = umc_get_valve(handle, params.dev, params.valve_channel)) < 0)
+                fprintf(stderr, "Get valve failed - %s\n", um_last_errorstr(handle));
+            else
+                printf("%d: Channel %d valve: %d\n", loop, params.valve_channel, ret);
+            if(!params.loop)
+                break;
+            if(loop < params.loop-1)
+                um_receive(handle, params.update);
+        }
+        while(++loop < params.loop);
+
+        if(ret >= 0 && params.value >= 0)
+        {
+            if((ret = umc_set_valve(handle, params.dev, params.valve_channel, params.value)) < 0)
+                fprintf(stderr, "Set valve failed - %s\n", um_last_errorstr(handle));
+            else
+                printf("Channel %d valve set: %d\n", params.valve_channel, params.value);
+        }
+        um_close(handle);
+        exit(ret>=0?0:-ret);
+    }
+
+    if(params.pressure_sensor)
+    {
+        do
+        {
+            if((ret = umc_measure_pressure(handle, params.dev, params.pressure_sensor, &pressure_kpa)) < 0)
+                fprintf(stderr, "Get pressure value failed - %s\n", um_last_errorstr(handle));
+            else
+                printf("%d: Channel %d pressure %1.3f kPa\n", loop, params.pressure_sensor, pressure_kpa);
+            if(!params.loop)
+                break;
+            if(loop < params.loop-1)
+                um_receive(handle, params.update);
+        }
+        while(++loop < params.loop);
+        um_close(handle);
+        exit(ret>=0?0:-ret);
+    }
+
+    if(params.read_fluid_detectors)
+    {
+        do
+        {
+            if((ret = umc_read_fluid_detectors(handle, params.dev)) < 0)
+                fprintf(stderr, "Read fluid detectors failed - %s\n", um_last_errorstr(handle));
+            else
+                printf("%d: Fluid detectors 0x%04x\n", loop, ret);
+            if(!params.loop)
+                break;
+            if(loop < params.loop-1)
+                um_receive(handle, params.update);
+        }
+        while(++loop < params.loop);
+        um_close(handle);
+        exit(ret>=0?0:-ret);
+    }
+
+
+    if(params.reset_fluid_detector)
+    {
+        if((ret = umc_reset_fluid_detector(handle, params.dev, params.reset_fluid_detector)) < 0)
+            fprintf(stderr, "reset fluid detector failed - %s\n", um_last_errorstr(handle));
+        else
+            printf("Channel %d fluid detector reset\n", params.reset_fluid_detector);
+
+        um_close(handle);
+        exit(ret>=0?0:-ret);
+    }
+
+    if(params.read_fluid_detectors)
+    {
+        do
+        {
+            if((ret = umc_read_fluid_detectors(handle, params.dev)) < 0)
+                fprintf(stderr, "Read fluid detectors failed - %s\n", um_last_errorstr(handle));
+            else
+                printf("%d: Fluid detectors 0x%04x\n", loop, ret);
+            if(!params.loop)
+                break;
+            if(loop < params.loop-1)
+                um_receive(handle, params.update);
+        }
+        while(++loop < params.loop);
+        um_close(handle);
+        exit(ret>=0?0:-ret);
+    }
+    if(params.calibrate_pressure)
+    {
+        do
+        {
+            ret = umc_pressure_calib(handle, params.dev, params.calibrate_pressure, params.update);
+            if(!params.loop)
+                break;
+            if(loop < params.loop-1)
+                um_receive(handle, params.update);
+        }
+        while(++loop < params.loop);
+        um_close(handle);
+        exit(ret>=0?0:ret);
+    }
+
+    // uMp or uMs goto position commands - or just reading current position
     if(um_get_positions(handle, params.dev, params.update, &home_x, &home_y, &home_z, &home_d, NULL) < 0)
     {
         fprintf(stderr, "Get positions failed - %s\n", um_last_errorstr(handle));
