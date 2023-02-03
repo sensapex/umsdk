716b540ec65eacacc0429282044744f76cfdc89a
Veli-Matti Kananen <veli-matti.kananen@acuvi.com>
2023-02-03 13:43:10 +0200

    Cross-compile for i686 windows by mingw

--- examples/cppsample/cppsample.cpp
+++ examples/cppsample/cppsample.cpp
@@ -273,7 +273,7 @@ void parse_args(int argc, char *argv[], Params &params)
     }
 }
 
-#if (defined(__APPLE__) && defined(__MACH__))
+#if (defined(WIN32) && !defined(WIN64))
 static int isnanf(const float arg)
 {
     return isnan(arg);
