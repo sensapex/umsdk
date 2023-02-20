a9099267ec72e3bd7b0dd03c033bef62bd3da7c6
Veli-Matti Kananen <veli-matti.kananen@acuvi.com>
2023-02-20 15:16:36 +0200

    a Few corrections in build and installation scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-BUILD_DIR="build"
-PROJECT_ROOT_PATH="$(pwd)/.."
+BUILD_DIR="build-Release"
+PROJECT_ROOT_PATH="$(dirname ${0})/.."
+
 BUILD_DIR_PATH="${PROJECT_ROOT_PATH}/${BUILD_DIR}"
 
 cd ${BUILD_DIR_PATH}
